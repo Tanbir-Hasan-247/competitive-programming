a332491fbc7ff3a4613cad0dbc6d04a02e7c4202
unknown <Tanbir-Hasan-247>
2025-12-11 05:03:00 +0600

    first

--- LIS_and_LDS.cpp
+++ LIS_and_LDS.cpp
@@ -1,8 +1,9 @@
 /**
- *   In the name of Allah, the Most Gracious, the Most Merciful.
- *   Author : Tanbir Hasan
- *   Time & Date : 21:01:00 10/12/2025
- **/
+*   Problem: PROBLEM_URL$
+*   In the name of Allah, the Most Gracious, the Most Merciful.
+*   Author : Tanbir Hasan
+*   Time & Date : 04:48:33 11/12/2025
+**/
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
@@ -11,7 +12,7 @@ using namespace std;
 #define maxVal(a) *max_element(a.begin(), a.end())
 #define minVal(a) *min_element(a.begin(), a.end())
 #define allr(a) a.rbegin(), a.rend()
-#define lcm(a, b) (a * b) / __gcd(a, b)
+#define lcm(a, b) (a*b) / __gcd(a, b)
 #define gcd(a, b) __gcd(a, b)
 #define all(a) a.begin(), a.end()
 #define ll long long
@@ -20,26 +21,7 @@ using namespace std;
 
 void solve()
 {
-    int n;
-
-    cin >> n;
-    vector<int> a(n);
-    map<int, int> mp;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
-        mp[a[i]]++;
-    }
-
-    int x = mp[1];
-    mp[1] = 0;
-    int y = 0;
-    for (auto val : mp)
-    {
-        y += val.second;
-    }
-
-    cout << y + (x + 1) / 2 << endl;
+    
 }
 
 int main()
